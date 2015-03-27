4e8af5b4c8571002ffe0f56c0a0e615cd6b9c8c5
Sven Anders Robbestad <robbestad@gmail.com>
2015-03-27 08:38:24 +0100

    Fixed error in nested route

--- index.jsx
+++ index.jsx
@@ -88,7 +88,7 @@ var Breadcrumbs = React.createClass({
                     separator = "";
 
                     if("undefined" != typeof _this.props.breadcrumbName){
-                        route.name=_this.props.breadcrumbName;
+                        //route.name=_this.props.breadcrumbName;
                         link=_this.props.breadcrumbName;
                     }
                 }

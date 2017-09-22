e6ec9371059cf22b24f677c669406d36548cbee1
Greg Venech <greg.venech@gmail.com>
2017-09-22 00:54:00 -0400

    refactor: rework and simplify build process
    Removed a lot of the eslint config and removed the
    `Makefile` in favor of `webpack` and npm `scripts`
    usage across the board. Also added `standard-version`
    and a `release` script.

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: '"production"' }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compressor: {
+        screw_ie8: true,
+        drop_console: true,
+        warnings: false
+      }
+    })
+  ],
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'prop-types': {
+      root: 'PropTypes',
+      commonjs2: 'prop-types',
+      commonjs: 'prop-types',
+      amd: 'prop-types'
+    },
+    'react-router-dom': {
+      root: 'ReactRouterDOM',
+      commonjs2: 'react-router-dom',
+      commonjs: 'react-router-dom',
+      amd: 'react-router-dom'
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'react-breadcrumbs.min.js'
+  }
+}
